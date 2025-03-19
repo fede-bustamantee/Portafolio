2acc169bc6c33a34cb56d2458059346d1fec3abc
fede bustamante <federicobustamantevyp@gmail.com>
2025-03-19 09:48:12 -0300

    inicio

--- src/app/componetes/HomePage.js
+++ src/app/componetes/HomePage.js
@@ -1,5 +1,5 @@
-"use client"; 
-import React, { useEffect } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "../styles/HomePage.css";
 import "aos/dist/aos.css"; // Importa el archivo CSS de AOS
 import AOS from "aos"; // Importa la librería de AOS
@@ -7,6 +7,8 @@ import personalData from "../data/personaldata";
 import contacto from "../data/contacto";
 
 const Home = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   useEffect(() => {
     // Inicializa AOS cuando el componente se monte
     AOS.init({
@@ -15,34 +17,39 @@ const Home = () => {
     });
   }, []); // El efecto se ejecuta solo una vez cuando el componente se monta
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
       <div className="textizquierda" data-aos-delay="100" data-aos="zoom-in">
-      <div className="lineavertical"></div>
+        <div className="lineavertical"></div>
         <a href={contacto.github}><i className="fab fa-github"></i></a>
         <a href={contacto.whatsapp}><i className="fab fa-whatsapp"></i></a>
         <a href={contacto.gmail}><i className="fa-regular fa-envelope"></i></a>
       </div>
 
       <div className="textderecha" data-aos-delay="100" data-aos="zoom-in">
-      <div className="linea-vertical"></div>
+        <div className="linea-vertical"></div>
         <p>fede</p>
       </div>
 
       <div className="secc" id="pagi">
         <nav>
-          <a href="/">
-            <h2 className="logo">
-              <span data-aos="zoom-in" data-aos-duration="1000">f</span>
-              <samp data-aos="zoom-in" data-aos-duration="1000">b</samp>
-            </h2>
+          <a href="/" className="logo">
+            <img src="/img/firme.png" alt="Logo" className="logo-img" data-aos="zoom-in" data-aos-duration="1000"/>
           </a>
 
-          <ul>
-            <li data-aos-delay="200" data-aos="zoom-in"><a href="#mi">01. Acerca de mi</a></li>
-            <li data-aos-delay="300" data-aos="zoom-in"><a href="#habi">02. Habilidades</a></li>
-            <li data-aos-delay="400" data-aos="zoom-in"><a href="#apps">03. Aplicaciones</a></li>
-            <li data-aos-delay="500" data-aos="zoom-in"><a href="#con">04. Contactame</a></li>
+          <div className="menu-icon" onClick={toggleMenu}>
+            <i className="fa-solid fa-bars"></i>
+          </div>
+
+          <ul className={menuOpen ? "nav-menu active" : "nav-menu"}>
+            <li data-aos-delay="200" data-aos="zoom-in"><a href="#mi" onClick={toggleMenu}>01. Acerca de mi</a></li>
+            <li data-aos-delay="300" data-aos="zoom-in"><a href="#habi" onClick={toggleMenu}>02. Habilidades</a></li>
+            <li data-aos-delay="400" data-aos="zoom-in"><a href="#apps" onClick={toggleMenu}>03. Aplicaciones</a></li>
+            <li data-aos-delay="500" data-aos="zoom-in"><a href="#con" onClick={toggleMenu}>04. Contactame</a></li>
           </ul>
 
           <div className="contenido">
--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{metadata.title}</title>
+        <link rel="icon" href="favicon.ico" />
         <meta name="description" content={metadata.description} />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
       </head>
